26e41f3fecd6477eb8b3c37f7966b96344f4e0a1
Eoin Campbell <campbeeo@tcd.ie>
2015-12-08 20:25:08 +0000

    Fix codechecker error, use POST not GET to upload Word file

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2015120801;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2015120802;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014051200;        // Requires Moodle 2.7 or higher, when Atto was added to core.
 $plugin->component = 'atto_wordimport';  // Full name of the plugin (used for diagnostics).
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.1.0 (Build 2015120801)'; // Human readable version information.
+$plugin->release   = '1.1.1 (Build 2015120802)'; // Human readable version information.
